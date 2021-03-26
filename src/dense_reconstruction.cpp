cf01eb669aacb3670d62c465a208f0ed1cedae8b
Varsha <varsha.kumar@outlook.com>
2021-03-25 21:59:44 -0400

    Edits to run on rectified images from CDR box dataset
    Changed from using compressed image messages as input rectified images to regular sensor_msgs/Image messages.

--- src/dense_reconstruction.cpp
+++ src/dense_reconstruction.cpp
@@ -2,7 +2,9 @@
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
+#include <sensor_msgs/CompressedImage.h>
 #include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/LaserScan.h>
 #include <sensor_msgs/ChannelFloat32.h>
 #include <geometry_msgs/Point32.h>
 #include <image_transport/image_transport.h>
@@ -12,21 +14,25 @@
 #include <fstream>
 #include <ctime>
 #include <opencv2/opencv.hpp>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include "elas.h"
-#include "popt_pp.h"
 
 using namespace cv;
 using namespace std;
 
+Mat img_left, img_right;
 Mat XR, XT, Q, P1, P2;
 Mat R1, R2, K1, K2, D1, D2, R;
 Mat lmapx, lmapy, rmapx, rmapy;
 Vec3d T;
 stereo_dense_reconstruction::CamToRobotCalibParamsConfig config;
 FileStorage calib_file;
-int debug = 0;
-Size out_img_size;
-Size calib_img_size;
+Size out_img_size(640, 480);
+Size calib_img_size(640, 480);
 
 image_transport::Publisher dmap_pub;
 ros::Publisher pcl_pub;
@@ -58,13 +64,16 @@ Mat composeTranslationCamToRobot(float x, float y, float z) {
   return (Mat_<double>(3,1) << x, y, z);
 }
 
+void debugCamToRobotTransformation(Mat& R, Mat& T) {
+  R = composeRotationCamToRobot(config.PHI_X,config.PHI_Y,config.PHI_Z);
+  T = 
+composeTranslationCamToRobot(config.TRANS_X,config.TRANS_Y,config.TRANS_Z);
+  cout << "Rotation: " << R << endl;
+  cout << "Translation: " << T << endl;
+}
+
 void publishPointCloud(Mat& img_left, Mat& dmap) {
-  if (debug == 1) {
-    XR = composeRotationCamToRobot(config.PHI_X,config.PHI_Y,config.PHI_Z);
-    XT = composeTranslationCamToRobot(config.TRANS_X,config.TRANS_Y,config.TRANS_Z);
-    cout << "Rotation matrix: " << XR << endl;
-    cout << "Translation matrix: " << XT << endl;
-  }
+  debugCamToRobotTransformation(XR, XT);
   Mat V = Mat(4, 1, CV_64FC1);
   Mat pos = Mat(4, 1, CV_64FC1);
   vector< Point3d > points;
@@ -110,11 +119,6 @@ void publishPointCloud(Mat& img_left, Mat& dmap) {
       ch.values.push_back(*reinterpret_cast<float*>(&rgb));
     }
   }
-  if (!dmap.empty()) {
-    sensor_msgs::ImagePtr disp_msg;
-    disp_msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", dmap).toImageMsg();
-    dmap_pub.publish(disp_msg);
-  }
   pc.channels.push_back(ch);
   pcl_pub.publish(pc);
 }
@@ -123,51 +127,71 @@ Mat generateDisparityMap(Mat& left, Mat& right) {
   if (left.empty() || right.empty()) 
     return left;
   const Size imsize = left.size();
-  const int32_t dims[3] = {imsize.width, imsize.height, imsize.width};
+  const int32_t dims[3] = {imsize.width,imsize.height,imsize.width};
   Mat leftdpf = Mat::zeros(imsize, CV_32F);
   Mat rightdpf = Mat::zeros(imsize, CV_32F);
 
-  Elas::parameters param(Elas::MIDDLEBURY);
+  Elas::parameters param;
   param.postprocess_only_left = true;
   Elas elas(param);
-  elas.process(left.data, right.data, leftdpf.ptr<float>(0), rightdpf.ptr<float>(0), dims);
-  Mat dmap = Mat(out_img_size, CV_8UC1, Scalar(0));
-  leftdpf.convertTo(dmap, CV_8U, 1.);
-  return dmap;
+  elas.process(left.data,right.data,leftdpf.ptr<float>(0),
+               rightdpf.ptr<float>(0),dims);
+  
+  Mat show = Mat(out_img_size, CV_8UC1, Scalar(0));
+  leftdpf.convertTo(show, CV_8U, 1.);
+  if (!show.empty()) {
+    sensor_msgs::ImagePtr disp_msg;
+    disp_msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", 
+show).toImageMsg();
+    dmap_pub.publish(disp_msg);
+  }
+  return show;
 }
 
-void imgCallback(const sensor_msgs::ImageConstPtr& msg_left, const sensor_msgs::ImageConstPtr& msg_right) {
-  Mat tmpL = cv_bridge::toCvShare(msg_left, "mono8")->image;
-  Mat tmpR = cv_bridge::toCvShare(msg_right, "mono8")->image;
-  if (tmpL.empty() || tmpR.empty())
+void imgCallback(const sensor_msgs::ImageConstPtr& msg_left, const 
+sensor_msgs::ImageConstPtr& msg_right) {
+  cout << "in image callback\n" << endl;
+  /* Mat img_left_color = cv::imdecode(cv::Mat(msg_left->data), 
+CV_LOAD_IMAGE_COLOR); */
+
+
+  cv_bridge::CvImagePtr cv_ptr_l, cv_ptr_r;
+  try
+  {
+    cv_ptr_l = cv_bridge::toCvCopy(msg_left, sensor_msgs::image_encodings::MONO8);
+    cv_ptr_r = cv_bridge::toCvCopy(msg_right, sensor_msgs::image_encodings::MONO8);
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
+
+  Mat img_left = cv_ptr_l->image; //cv::imdecode(cv::Mat(msg_left->data), CV_LOAD_IMAGE_GRAYSCALE);
+  Mat img_right = cv_ptr_r->image; //cv::imdecode(cv::Mat(msg_right->data), CV_LOAD_IMAGE_GRAYSCALE);
+  
+  cout << "tried to load images \n" << endl;
+  if (img_left.empty() || img_right.empty())
     return;
   
-  Mat img_left, img_right, img_left_color;
-  remap(tmpL, img_left, lmapx, lmapy, cv::INTER_LINEAR);
-  remap(tmpR, img_right, rmapx, rmapy, cv::INTER_LINEAR);
-  
-  cvtColor(img_left, img_left_color, CV_GRAY2BGR);
-  
   Mat dmap = generateDisparityMap(img_left, img_right);
-  publishPointCloud(img_left_color, dmap);
-  
-  imshow("LEFT", img_left);
-  imshow("RIGHT", img_right);
-  imshow("DISP", dmap);
-  waitKey(30);
+
+  cout << "Disparity Map Generated\n" << endl;
+  //publishPointCloud(img_left_color, dmap);
 }
 
 void findRectificationMap(FileStorage& calib_file, Size finalSize) {
   Rect validRoi[2];
-  cout << "Starting rectification" << endl;
+  cout << "starting rectification" << endl;
   stereoRectify(K1, D1, K2, D2, calib_img_size, R, Mat(T), R1, R2, P1, P2, Q, 
-                CV_CALIB_ZERO_DISPARITY, 0, finalSize, &validRoi[0], &validRoi[1]);
-  cv::initUndistortRectifyMap(K1, D1, R1, P1, finalSize, CV_32F, lmapx, lmapy);
-  cv::initUndistortRectifyMap(K2, D2, R2, P2, finalSize, CV_32F, rmapx, rmapy);
-  cout << "Done rectification" << endl;
+                CV_CALIB_ZERO_DISPARITY, 0, finalSize, &validRoi[0], 
+&validRoi[1]);
+  cout << "done rectification" << endl;
 }
 
-void paramsCallback(stereo_dense_reconstruction::CamToRobotCalibParamsConfig &conf, uint32_t level) {
+void paramsCallback(stereo_dense_reconstruction::CamToRobotCalibParamsConfig 
+&conf, uint32_t level) {
+  cout << "in params callback" << endl;
   config = conf;
 }
 
@@ -176,32 +200,7 @@ int main(int argc, char** argv) {
   ros::NodeHandle nh;
   image_transport::ImageTransport it(nh);
   
-  const char* left_img_topic;
-  const char* right_img_topic;
-  const char* calib_file_name;
-  int calib_width, calib_height, out_width, out_height;
-  
-  static struct poptOption options[] = {
-    { "left_topic",'l',POPT_ARG_STRING,&left_img_topic,0,"Left image topic name","STR" },
-    { "right_topic",'r',POPT_ARG_STRING,&right_img_topic,0,"Right image topic name","STR" },
-    { "calib_file",'c',POPT_ARG_STRING,&calib_file_name,0,"Stereo calibration file name","STR" },
-    { "calib_width",'w',POPT_ARG_INT,&calib_width,0,"Calibration image width","NUM" },
-    { "calib_height",'h',POPT_ARG_INT,&calib_height,0,"Calibration image height","NUM" },
-    { "out_width",'u',POPT_ARG_INT,&out_width,0,"Rectified image width","NUM" },
-    { "out_height",'v',POPT_ARG_INT,&out_height,0,"Rectified image height","NUM" },
-    { "debug",'d',POPT_ARG_INT,&debug,0,"Set d=1 for cam to robot frame calibration","NUM" },
-    POPT_AUTOHELP
-    { NULL, 0, 0, NULL, 0, NULL, NULL }
-  };
-
-  POpt popt(NULL, argc, argv, options, 0);
-  int c;
-  while((c = popt.getNextOpt()) >= 0) {}
-  
-  calib_img_size = Size(calib_width, calib_height);
-  out_img_size = Size(out_width, out_height);
-  
-  calib_file = FileStorage(calib_file_name, FileStorage::READ);
+  /*calib_file = FileStorage(argv[1], FileStorage::READ);
   calib_file["K1"] >> K1;
   calib_file["K2"] >> K2;
   calib_file["D1"] >> D1;
@@ -210,24 +209,34 @@ int main(int argc, char** argv) {
   calib_file["T"] >> T;
   calib_file["XR"] >> XR;
   calib_file["XT"] >> XT;
+  findRectificationMap(calib_file, out_img_size);*/
   
-  findRectificationMap(calib_file, out_img_size);
+  message_filters::Subscriber<sensor_msgs::Image> sub_img_left(nh, 
+"/stereo/left/image_rect", 1);
+  message_filters::Subscriber<sensor_msgs::Image> sub_img_right(nh, 
+"/stereo/right/image_rect", 1);
+
   
-  message_filters::Subscriber<sensor_msgs::Image> sub_img_left(nh, left_img_topic, 1);
-  message_filters::Subscriber<sensor_msgs::Image> sub_img_right(nh, right_img_topic, 1);
-  
-  typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> SyncPolicy;
-  message_filters::Synchronizer<SyncPolicy> sync(SyncPolicy(10), sub_img_left, sub_img_right);
+  typedef 
+message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, 
+sensor_msgs::Image> SyncPolicy;
+  message_filters::Synchronizer<SyncPolicy> sync(SyncPolicy(10), 
+sub_img_left, sub_img_right);
   sync.registerCallback(boost::bind(&imgCallback, _1, _2));
   
-  dynamic_reconfigure::Server<stereo_dense_reconstruction::CamToRobotCalibParamsConfig> server;
-  dynamic_reconfigure::Server<stereo_dense_reconstruction::CamToRobotCalibParamsConfig>::CallbackType f;
+  dynamic_reconfigure::Server<stereo_dense_reconstruction::
+CamToRobotCalibParamsConfig> server;
+  dynamic_reconfigure::Server<stereo_dense_reconstruction::
+CamToRobotCalibParamsConfig>::CallbackType f;
 
   f = boost::bind(&paramsCallback, _1, _2);
   server.setCallback(f);
   
-  dmap_pub = it.advertise("/camera/left/disparity_map", 1);
-  pcl_pub = nh.advertise<sensor_msgs::PointCloud>("/camera/left/point_cloud",1);
+  dmap_pub = it.advertise("/camera_left_rect/disparity_map", 1);
+  pcl_pub = 
+nh.advertise<sensor_msgs::PointCloud>("/camera_left_rect/point_cloud",1);
+
+  cout << "about to spin \n" ;
 
   ros::spin();
   return 0;
